3ae1a9856540e9c1b2f55092f30a6c3568291de2
F1LT3R <al@pwn.io>
2018-04-07 01:40:16 -0400

    Add documentation.

--- hello-world.js
+++ hello-world.js
@@ -2,9 +2,20 @@ const fs = require('fs')
 const chalk = require('chalk')
 const ansiToSVG = require('.')
 
-const ansiText = chalk`{green 👋 Hello}, {blueBright World} 🌏{redBright !}`
+const colorFile = './fixtures/base16-flat-dark-f1lt3r-256.itermcolors'
+const ansiText = chalk`{green 👋 Hello}, {blueBright World} 🌏{redBright !}\n` +
+	chalk.bgRed('👋') +
+	chalk.bgYellow('🦄') +
+	chalk.bgGreen('🐘') + ' ' +
+	chalk.strikethrough.italic('13') +
+	chalk.bold('3') + chalk.underline('7') + ' ' +
+	chalk.bgCyan('🍄') +
+	chalk.bgBlue('🎃') +
+	chalk.bgMagenta('🐦')
+
 const result = ansiToSVG(ansiText, {
-	fontFamily: 'Courier'
+	fontFamily: 'Courier',
+	colors: colorFile
 })
 const outputFile = './examples/hello-world.svg'
 fs.writeFileSync(outputFile, result)
